bd67334622a8ab6ac663456c73281221e798cb06
Muzammil Mehmood <gmuzammil555@gmail.com>
2026-03-10 19:10:39 +0500

    Add files via upload

---
+++ 01 Basics/05. Size of all Data Types .cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+
+int main ()
+{
+   cout<<" Size of all Data Types : "<<endl;
+   cout<<" size of int : "<<sizeof(int)<<" bytes"<<endl;
+   cout<<" size of float : "<<sizeof(float)<<" bytes "<<endl;
+   cout<<" size of char : "<<sizeof(char)<<" byte"<<endl;   cout<<" size of double : "<<sizeof(double)<<" bytes"<<endl;
+   cout<<" size of bool : "<<sizeof(bool)<<" byte";
+    
+    return 0;
+}
