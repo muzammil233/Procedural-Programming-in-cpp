11b27f509d09e59b243055453b15091f428a8ecc
Muzammil Mehmood <gmuzammil555@gmail.com>
2026-04-09 19:19:13 +0500

    Add files via upload

---
+++ 02. Conditional Statements/01. Check a negative number (only).cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int num;
+	cout<<"Enter a Number: ";
+	cin>>num;
+
+	if(num<0)
+	{
+		cout<<num<<" is a negative number.";
+	}
+
+	return 0;
+}
